c859405decdc6e24cb628262f9e5161296b58067
MikeIv <210415@dantser.ru>
2021-06-21 17:25:34 +0300

    add chat

--- tasks/style.js
+++ tasks/style.js
@@ -6,18 +6,12 @@ const sass = require('gulp-dart-sass');
 const bulk = require('gulp-sass-bulk-importer');
 const prefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean-css');
-const cleancss = require('gulp-clean');
 const concat = require('gulp-concat');
 const map = require('gulp-sourcemaps');
 const bs = require('browser-sync');
 
-const timestamp = (new Date()).getTime();
-const rename = require("gulp-rename");
-
 
 module.exports = function style() {
-				src('build/css')
-					.pipe(cleancss())
 	return src('src/scss/**/*.scss')
 		.pipe(map.init())
 		.pipe(bulk())
@@ -42,12 +36,6 @@ module.exports = function style() {
 		.pipe(concat('style.min.css'))
 		.pipe(map.write('../sourcemaps/'))
 		.pipe(dest('build/css/'))
-
-			.pipe(rename(function (path) {
-				if (path.basename === 'style.min') {
-					path.basename += '-' + timestamp;
-				}
-			}))
 		.pipe(dest(function (file) {
 			return file.base;
 		}))
